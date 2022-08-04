f8fbf77dcb8304f85348ecc113dba65e97efb06d
Enzolachox <96921333+Enzolachox@users.noreply.github.com>
2022-08-04 11:35:19 -0500

    update OTRA VES
    cambios 2

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1 ,something;
+var box1, pig1 ,something,nothing;
 var backgroundImg;
 
 function preload() {
@@ -22,7 +22,8 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-something = createSprite(600,200)
+something = createSprite(600,200);
+nothing = createSprite(10,10);
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
